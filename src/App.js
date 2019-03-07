40b0cb6fa5542b3f16df5c84b9f57421f0fa89bd
Jakub Kicek <j.kicek@gmail.com>
2019-03-07 03:08:10 +0100

    full weapon database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import weapons from './weapons.json';
+import sM from './simpleMelee.json';
+import sR from './simpleRanged.json';
+import mM from './martialMelee.json';
+import mR from './martialRanged.json';
 import WeaponList from './WeaponList.js';
 
 
@@ -9,34 +12,14 @@ class App extends Component {
     constructor(props){
       super(props)
       this.state = {
-          searchphrase: " ",
+          weaponBase: sM.concat(sR, mM, mR)
       };
-//      weapons.forEach(w => {
-//        const wItem = <WeaponItem key={w.id} id={w.id} cost={w.cost} damage_dice={w.damage_dice} damage_type={w.damage_type} weight={w.weight} properties={w.properties}></WeaponItem>
-//        this.state.resultsList.push(wItem);
     }  
-  
-//    filterResults(event){        
-//        weapons.forEach(w => {
-//        if(w.id.includes(event.target.value+'')){
-//            const wItem = <WeaponItem key={w.id} id={w.id} cost={w.cost} damage_dice={w.damage_dice} damage_type={w.damage_type} weight={w.weight} properties={w.properties}></WeaponItem>
-//            this.state.resultsList.push(wItem);
-//        }
-//    })
-//    }
-//    
-//    componentDidMount(){
-//              weapons.forEach(w => {
-//        const wItem = <WeaponItem key={w.id} id={w.id} cost={w.cost} damage_dice={w.damage_dice} damage_type={w.damage_type} weight={w.weight} properties={w.properties}></WeaponItem>
-//        this.state.resultsList.push(wItem);
-//    }) 
-//    }
-//    
     
     render(){
     return (
       <div className="App">
-        <WeaponList items={weapons}></WeaponList>
+        <WeaponList items={this.state.weaponBase}></WeaponList>
       </div>
     );
   }

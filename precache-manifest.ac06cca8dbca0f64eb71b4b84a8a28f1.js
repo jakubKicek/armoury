d478ba3096a632fba09c9fd257e88942076bf3f7
Jakub Kicek <j.kicek@gmail.com>
2019-03-07 12:11:14 +0100

    Updates

---
+++ precache-manifest.ac06cca8dbca0f64eb71b4b84a8a28f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d436f8881bb517f7f71",
+    "url": "/dnd_5e_armoury/static/js/runtime~main.5d436f88.js"
+  },
+  {
+    "revision": "098c3791fe1047badf79",
+    "url": "/dnd_5e_armoury/static/js/main.098c3791.chunk.js"
+  },
+  {
+    "revision": "91c28e1e164634d9b62e",
+    "url": "/dnd_5e_armoury/static/js/2.91c28e1e.chunk.js"
+  },
+  {
+    "revision": "098c3791fe1047badf79",
+    "url": "/dnd_5e_armoury/static/css/main.6badde80.chunk.css"
+  },
+  {
+    "revision": "0ab26d213cbed93098f04fc51d250c3b",
+    "url": "/dnd_5e_armoury/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dnd_5e_armoury/precache-manifest.ac06cca8dbca0f64eb71b4b84a8a28f1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dnd_5e_armoury/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
